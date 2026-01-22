d422aebd87eec6a45f3e2627352f90070a9cb9c5
StrawHat-Luffyyy <krishmac1209@gmail.com>
2026-01-22 20:54:48 +0530

    feat: update user preferences import path and increase worker attempts to 10

--- event-notification-service/src/workers/event.worker.js
+++ event-notification-service/src/workers/event.worker.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "../config/db.js";
 import Event from "../modules/events/events.model.js";
 import Notification from "../modules/notifications/notification.model.js";
-import UserPreferences from "../modules/userPreferences/userPreferences.model.js";
+import UserPreferences from "../modules/preferences/preference.model.js";
 import DeliveryLog from "../modules/deliveryLogs/deliveryLog.model.js";
 import { getNotificationContent } from "../utils/notificationTemplates.js";
 
@@ -75,9 +75,10 @@ const eventWorker = new Worker(
     event.status = "PROCESSED";
     await event.save();
   },
+  // Worker options
   {
     connection: redis,
-    attempts: 3,
+    attempts: 10,
     backoff: {
       type: "exponential",
       delay: 2000,
@@ -101,3 +102,33 @@ eventWorker.on("failed", async (job, err) => {
 });
 
 export default eventWorker;
+
+
+/*
+Flow Explanation:
+We are importing Worker from bullmq and other necessary modules like mongoose models and config files.
+We are configuring dotenv and connecting to the database.
+Now we are creating a new Worker named eventWorker which listens to the eventQueue
+first check if that eventtype is processEvent then we will proceed further or we'll return
+then take eventId from job.data
+check if eventID is present or not if not we'll throw an error
+Now we'll check if userPref is there or not from eventId if not  we'll create a new pref
+So now if preference is there so that we'll check that if that event is Enabled or not 
+if not then we'll save that event as processes and we'll save it and return 
+so now if the event is enabled we'll send a notification
+so we'll destructure title and message from getNotificationtemplate(eventType , payload)
+we'll create a empty array of channels
+Now we'll check if (preference.channels.InApp) channels.push("IN_APP") similarly for the other channels too.
+Now we'll create a for loop[const channel from channels] in which we'll create Notifications and DeliveryLogs
+we'll start inside the loop with a variable named successfulChannels which is initialzed from 0 to keep the count of how many channels are successfully executed and this is outside the trycatch block.
+Now we'll wrap it with trycatch because of the db operation
+first we'll create Notification followed by DeliveryLogs in the try block and after the creation of the deliveylog we'll increase the value of the successfulChannels++
+and in the catch block we'll do DeliveryLog with a status FAILED
+and now we are out of the trycatch and loop and we'll check if successfulChannels === 0 then we'll return a Error mesaage saying all the channels failed to create the notification
+Now we'll marked the event as PROCESSED and save it in and return
+Now we'll configure worker options like connection , attempts and backoff{ type-exponential and delay }
+Now we are out of the eventWorker and we'll handle what to do when the job succeeds or fails 
+if it succeeds we'll do a console log job completed with job id
+if it fails we'll do console.log Job failed 
+and If it fails too many times → mark the related event as FAILED in the database  
+*/
